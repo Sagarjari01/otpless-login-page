79d9bf7e3e571221a6a8c0dbdf6d0d8c971ea307
Sagarjari01 <sagarjari9495@gmail.com>
2023-11-17 15:10:26 +0530

    fixed routing

--- src/utills/initOtpless.js
+++ src/utills/initOtpless.js
@@ -1,14 +1,14 @@
-import { json, useNavigate } from "react-router-dom";
-
-const loadScript = () =>{
-    const script = document.createElement("script")
-    script.src = "https://otpless.com/auth.js";
-    script.id = "otplessIdScript";
-    document.body.appendChild(script)
-}
-
 export const initOTPless = () => {
+
     const otplessInit = Reflect.get(window, 'otplessInit');
+
+    const loadScript = () =>{
+        const script = document.createElement("script")
+        script.src = "https://otpless.com/auth.js";
+        script.id = "otplessIdScript";
+        document.body.appendChild(script)
+    }
+
     if (otplessInit) {
         otplessInit()
     }
@@ -17,11 +17,10 @@ export const initOTPless = () => {
     }
 
     window.otpless = (otplessUser) => {
-        const navigate = useNavigate
         const currentURL = window.location.href;
         const urlWithoutParams = currentURL.split("?")[0];
         window.history.pushState(null, "", urlWithoutParams);
         alert("OTPLESS DATA :"+JSON.stringify(otplessUser))
-        navigate('/dashboard')
+        window.location.href = '/dashboard';
     };
 }
